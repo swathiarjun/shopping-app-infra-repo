53ae3775747f83c2d14f749c45ab5a02a8911d50
swathi <swathi@gmail.com>
2023-12-28 11:38:56 +0000

    prometheus port opened

--- main.tf
+++ main.tf
@@ -37,6 +37,14 @@ resource "aws_security_group" "frontend" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
